d3d223181cb01e111edfdfe16beea4488cd33c85
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-01-04 14:02:57 +0100

    Init

---
+++ LSB steganography in C++ and x64 assembly/ASM.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifndef ASM_ASM
+#define ASM_ASM
+
+extern "C" void AdjustBrightnessASM(unsigned char* bmpDataScan0, unsigned char* bmpOriginal, int imageSizeInBytesunsigned, unsigned char* text, int textSize);
+#endif // !ASM_ASM
